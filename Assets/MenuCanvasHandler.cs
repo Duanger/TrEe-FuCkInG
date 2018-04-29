e4851716cf1cdcb4ec506cf45fc57c8431fc9230
Duanger <aaronshenglu@gmail.com>
2018-04-29 02:42:49 -0400

    Created prototype meny screen and started adding dialogue to convo 1

---
+++ Assets/MenuCanvasHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvasHandler : MonoBehaviour
+{
+
+	public float _timeToChangeScene;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClickPlay()
+	{
+			StartCoroutine(WaitAndChange(_timeToChangeScene));
+	}
+
+	IEnumerator WaitAndChange(float time)
+	{
+		yield return new WaitForSeconds(time);
+		SceneManager.LoadScene(1);
+	}
+}
