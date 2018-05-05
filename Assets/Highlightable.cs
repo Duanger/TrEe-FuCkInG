7480acf01eee7ef0f494e9c5621bcbb6a1b7561a
DESKTOP-4FRHUOP\Lu Huangdi <aaronshenglu@gmail.com>
2018-05-05 00:13:31 -0400

    highlighting works now

---
+++ Assets/Highlightable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Highlightable : MonoBehaviour
+{
+
+	public Sprite HighLightSprite;
+
+	private Button _butt;
+
+	private Color _whiteness;
+	// Use this for initialization
+	void Start ()
+	{
+		_butt = GetComponent<Button>();
+_whiteness = Color.white;
+		_whiteness.a = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_butt.image.color = _whiteness;
+	}
+
+	public void death()
+	{
+		_whiteness.a = 12;
+	
+	}
+}
